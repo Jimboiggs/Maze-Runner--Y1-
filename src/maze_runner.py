ce5548ef930cf6bdc2a12cf1d7fa41fc92457d99
Jimboiggs <jamesworsley07@gmail.com>
2025-11-24 19:06:56 +0000

    Task 4 (shortest path algorithm)

--- src/maze_runner.py
+++ src/maze_runner.py
@@ -1,5 +1,9 @@
 from typing import Optional
 
+from maze import create_maze
+from runner import explore
+from runner import create_runner
+
 
 def shortest_path(
     maze, starting: Optional[tuple[int, int]], goal: Optional[tuple[int, int]]
@@ -8,3 +12,18 @@ def shortest_path(
         starting = (0, 0)
     if goal is None:
         goal = (maze["width"] - 1, maze["height"] - 1)
+
+    runner = create_runner(starting[0], starting[1])
+    path = explore(runner, maze, goal)
+    print(path)
+
+    previous = []
+    for i in range(len(path)):
+        if path[i] not in previous:
+            previous.append(path[i])
+        else:
+            j = previous.index(path[i])
+            for k in range(j - i):
+                del path[j - k]
+
+    return path
