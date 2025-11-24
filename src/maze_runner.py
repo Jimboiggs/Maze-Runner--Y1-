6dd0f06acd31cadd48f9f15ddf04b325a27a00eb
Jimboiggs <jamesworsley07@gmail.com>
2025-11-24 22:05:52 +0000

    Created maze_reader
    Creates a maze by reading from a file

--- src/maze_runner.py
+++ src/maze_runner.py
@@ -1,6 +1,5 @@
 from typing import Optional
 
-from maze import create_maze
 from runner import explore
 from runner import create_runner
 
@@ -27,3 +26,33 @@ def shortest_path(
                 del path[j - k]
 
     return path
+
+
+def maze_reader(maze_file: str) -> dict:
+    try:
+        with open(maze_file) as f:
+            height = 0
+            width = 0
+            first = f.readline().rstrip()
+            for line in f:
+                height += 1
+                width = len(line)
+                last = line
+
+            # Check outer walls
+            last = last.rstrip()
+            for j in range(len(last)):
+                if first[j] != "#" or last[j] != "#":
+                    raise ValueError("Maze must be enclosed with walls")
+            for k in f:
+                if k[0] != "#" or k[-1] != "#":
+                    raise ValueError("Maze must be enclosed with walls")
+
+            maze = {
+                "height": height,
+                "width": width,
+            }
+            return maze
+
+    except:
+        raise IOError("Error reading maze file")
